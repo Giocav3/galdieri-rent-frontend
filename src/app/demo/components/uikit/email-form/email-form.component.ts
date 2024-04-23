f1fe6c998f079b56561c82dfafa877ca17cc688b
giovannicavaliere3 <giocavaliere.3@gmail.com>
2024-04-23 09:26:35 +0200

    feat: :art: Improved email-form with editor and validators in the form, with 2 svgs

--- src/app/demo/components/uikit/email-form/email-form.component.ts
+++ src/app/demo/components/uikit/email-form/email-form.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AfterViewInit, Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import Quill from 'quill';
 
 @Component({
   selector: 'app-email-form',
@@ -7,19 +8,40 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrl: './email-form.component.scss'
 })
 export class EmailFormComponent {
+  destinatario: string;
+  oggetto: string;
+  messaggio: string;
+  text: string;
+  cc: string
   emailForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
-    this.emailForm = this.fb.group({
-      to: ['', [Validators.required, Validators.email]],
-      subject: ['', Validators.required],
-      message: ['']
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.emailForm = new FormGroup({
+      destinatario: new FormControl('', [Validators.required, Validators.email]),
+      oggetto: new FormControl('', [Validators.required, Validators.minLength(1)]),
+      cc: new FormControl('', [Validators.email]),
+      text: new FormControl(''),
     });
   }
 
-  sendEmail() {
-    // Implementa il codice per inviare l'email qui
-    console.log(this.emailForm.value);
+  inviaEmail() {
+    if (this.emailForm.valid) {
+      // Invia l'email solo se il form è valido
+      console.log('Invio email a:', this.emailForm.value.destinatario);
+      console.log('Oggetto:', this.emailForm.value.oggetto);
+      console.log('CC:', this.emailForm.value.cc);
+      console.log('Messaggio:', this.emailForm.value.text);
+      // Resetta il form dopo l'invio
+      this.resetForm();
+    } else {
+      // Gestisci il caso in cui il form non è valido, ad esempio mostrando un messaggio all'utente
+    }
+  }
+
+  resetForm() {
+    this.emailForm.reset();
   }
 
 }
--- src/app/demo/components/uikit/email-form/email-form.module.ts
+++ src/app/demo/components/uikit/email-form/email-form.module.ts
@@ -1,21 +1,26 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { EmailFormComponent } from "./email-form.component";
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { EditorModule } from 'primeng/editor';
 import { EmailFormRoutingModule } from "./email-form-routing.module";
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     imports: [
         CommonModule,
         FormsModule,
-        ReactiveFormsModule,
         InputTextModule,
         ButtonModule,
         EditorModule,
         EmailFormRoutingModule,
+        InputGroupModule,
+        InputGroupAddonModule,
+        ReactiveFormsModule
     ],
     exports: [EmailFormComponent],
     declarations: [EmailFormComponent]
